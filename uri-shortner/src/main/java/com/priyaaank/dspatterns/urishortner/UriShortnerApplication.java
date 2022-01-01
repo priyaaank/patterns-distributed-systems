a46a266e66dd84887eddc58f75c666cb761ddd81
Priyank Gupta <priyaaank@gmail.com>
2022-01-01 15:33:17 +0530

    Added property to toggle between batch mode and oltp mode

--- uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/UriShortnerApplication.java
+++ uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/UriShortnerApplication.java
@@ -2,12 +2,14 @@ package com.priyaaank.dspatterns.urishortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UriShortnerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UriShortnerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UriShortnerApplication.class, args);
+    }
 
 }
--- uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/bookmarks/jobs/UrlDetailsPopulateBatchJob.java
+++ uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/bookmarks/jobs/UrlDetailsPopulateBatchJob.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,17 +21,25 @@ import java.io.InputStreamReader;
 public class UrlDetailsPopulateBatchJob {
 
     private String batchFileName;
+    private Boolean rateControlledExecution;
+    private Boolean batchMode;
     private UrlTextExtractorService enrichBookmarksService;
     private ResourceLoader resourceLoader;
+    private RestTemplate restTemplate;
     private DeferredResultRegistry<String> deferredResultRegistry;
 
     @Autowired
     public UrlDetailsPopulateBatchJob(@Value("${batchFilePath}") String batchFileName,
+                                      @Value("${extract.text.rateControl}") Boolean rateControlledExecution,
+                                      @Value("${extract.text.mode.batch}") Boolean batchMode,
                                       UrlTextExtractorService urlTextExtractorService,
                                       ResourceLoader resourceLoader) {
         this.batchFileName = batchFileName;
+        this.rateControlledExecution = rateControlledExecution;
+        this.batchMode = batchMode;
         this.enrichBookmarksService = urlTextExtractorService;
         this.resourceLoader = resourceLoader;
+        this.restTemplate = new RestTemplate();
     }
 
     public void triggerJob() throws IOException {
@@ -38,9 +48,23 @@ public class UrlDetailsPopulateBatchJob {
         Resource resource = this.resourceLoader.getResource("classpath:" + this.batchFileName);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
         while ((line = bufferedReader.readLine()) != null) {
-            this.enrichBookmarksService.extractText(new UrlTextExtractorService.ExtractionRequest<>(System.currentTimeMillis(), new Url(line)));
+            if (this.batchMode) {
+                extractWithLocalCall(line);
+            } else {
+                extractWithHttpCall(line);
+            }
         }
         log.info("I have queued up all results!");
+    }
+
+    private void extractWithHttpCall(String url) {
+        String textUrl = "http://bookmarksmanager:8080/bookmark/enrich?url=" + url + "&fieldsRequested=text";
+        ResponseEntity<String> textResponse = restTemplate.getForEntity(textUrl, String.class);
+        log.info("Response for {} was {}", url, textResponse.getBody());
+    }
+
+    private void extractWithLocalCall(String line) {
+        this.enrichBookmarksService.extractText(new UrlTextExtractorService.ExtractionRequest<>(System.currentTimeMillis(), new Url(line)));
     }
 
 }
