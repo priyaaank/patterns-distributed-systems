d949b00c38d0e03aa19f04618b9ca0547ac0e0c1
Priyank Gupta <priyaaank@gmail.com>
2022-01-08 16:01:43 +0530

    Keeping minimum code needed to demonstrate transient failures

--- bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/BookmarkTagsResolver.java
+++ bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/BookmarkTagsResolver.java
@@ -31,9 +31,7 @@ public class BookmarkTagsResolver {
 
     public Bookmark fetchTags(Bookmark bookmark) {
         String url = urlServiceHost + "/tags/generate?url=" + bookmark.getLongUrl();
-        ResponseEntity<BookmarksTagsResponse> tagsResponse = retryHandler.callWithRetry(() -> {
-            return this.restTemplate.getForEntity(url, BookmarksTagsResponse.class);
-        });
+        ResponseEntity<BookmarksTagsResponse> tagsResponse = this.restTemplate.getForEntity(url, BookmarksTagsResponse.class);
         List<String> tags = tagsResponse == null ? new ArrayList<>() : tagsResponse.getBody().getTags();
         return bookmark.cloneBuilder().tags(tags).build();
     }
--- bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/EnrichBookmarksService.java
+++ bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/EnrichBookmarksService.java
@@ -2,10 +2,8 @@ package com.priyaaank.dspatterns.bookmarksmanager.bookmarks.service;
 
 import com.priyaaank.dspatterns.bookmarksmanager.bookmarks.domain.Bookmark;
 import com.priyaaank.dspatterns.bookmarksmanager.bookmarks.domain.BookmarkFieldSelector;
-import com.priyaaank.dspatterns.bookmarksmanager.circuitbreaker.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,39 +12,16 @@ import java.util.List;
 @Slf4j
 public class EnrichBookmarksService {
 
-    private BookmarkShorteningService bookmarkShorteningService;
-    private BookmarkTitleResolver titleResolver;
-    private BookmarkTextResolver textResolver;
-    private CircuitBreaker<Bookmark, Bookmark> tagsResolverCircuit;
+    private BookmarkTagsResolver bookmarkTagsResolver;
 
     @Autowired
-    public EnrichBookmarksService(BookmarkShorteningService bookmarkShorteningService,
-                                  BookmarkTitleResolver titleResolver,
-                                  BookmarkTagsResolver bookmarkTagsResolver,
-                                  BookmarkTextResolver textResolver,
-                                  @Value("${circuitbreaker.enabled}") Boolean isCircuitBreakerEnabled,
-                                  @Value("${circuitbreaker.failOver.enabled}") Boolean isFailOverEnabled) {
-        this.bookmarkShorteningService = bookmarkShorteningService;
-        this.titleResolver = titleResolver;
-        this.textResolver = textResolver;
-        this.tagsResolverCircuit = isFailOverEnabled ?
-                new CircuitBreaker<>(isCircuitBreakerEnabled, bookmarkTagsResolver::fetchTags, bookmarkTagsResolver::generateTagsLocally) :
-                new CircuitBreaker<>(isCircuitBreakerEnabled, bookmarkTagsResolver::fetchTags);
+    public EnrichBookmarksService(BookmarkTagsResolver bookmarkTagsResolver) {
+        this.bookmarkTagsResolver = bookmarkTagsResolver;
     }
 
     public Bookmark enrichBookmark(String fieldsRequested, Bookmark bookmark) {
         BookmarkFieldSelector fieldSelector = new BookmarkFieldSelector(fieldsRequested);
-        String updatedShortUrl = fieldSelector.enrichShortUrl(() -> this.bookmarkShorteningService.shorten(bookmark));
-        String updatedTitle = fieldSelector.enrichTitle(() -> this.titleResolver.fetchTitle(bookmark));
-        String updatedText = fieldSelector.enrichText(() -> this.textResolver.fetchText(bookmark));
-        List<String> updatedTags = fieldSelector.enrichTags((b) -> tagsResolverCircuit.check(b), bookmark);
-
-        return Bookmark.builder()
-                .shortenedUrl(updatedShortUrl)
-                .title(updatedTitle)
-                .text(updatedText)
-                .tags(updatedTags)
-                .longUrl(bookmark.getLongUrl())
-                .build();
+        List<String> updatedTags = fieldSelector.enrichTags((b) -> bookmarkTagsResolver.fetchTags(b), bookmark);
+        return Bookmark.builder().tags(updatedTags).longUrl(bookmark.getLongUrl()).build();
     }
 }
