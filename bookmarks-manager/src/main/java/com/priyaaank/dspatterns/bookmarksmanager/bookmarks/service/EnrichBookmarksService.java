2a9562725a793ee30855a03bb28121f3756a94d3
Priyank Gupta <priyaaank@gmail.com>
2022-01-08 23:16:15 +0530

    Added base circuite breaking capabilities

--- bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/EnrichBookmarksService.java
+++ bookmarks-manager/src/main/java/com/priyaaank/dspatterns/bookmarksmanager/bookmarks/service/EnrichBookmarksService.java
@@ -2,8 +2,10 @@ package com.priyaaank.dspatterns.bookmarksmanager.bookmarks.service;
 
 import com.priyaaank.dspatterns.bookmarksmanager.bookmarks.domain.Bookmark;
 import com.priyaaank.dspatterns.bookmarksmanager.bookmarks.domain.BookmarkFieldSelector;
+import com.priyaaank.dspatterns.bookmarksmanager.circuitbreaker.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,16 +14,20 @@ import java.util.List;
 @Slf4j
 public class EnrichBookmarksService {
 
-    private BookmarkTagsResolver bookmarkTagsResolver;
+    private CircuitBreaker<Bookmark, Bookmark> tagsResolverCircuit;
 
     @Autowired
-    public EnrichBookmarksService(BookmarkTagsResolver bookmarkTagsResolver) {
-        this.bookmarkTagsResolver = bookmarkTagsResolver;
+    public EnrichBookmarksService(BookmarkTagsResolver bookmarkTagsResolver,
+                                  @Value("${circuitbreaker.enabled}") Boolean isCircuitBreakerEnabled,
+                                  @Value("${circuitbreaker.failOver.enabled}") Boolean isFailOverEnabled) {
+        this.tagsResolverCircuit = isFailOverEnabled ?
+                new CircuitBreaker<>(isCircuitBreakerEnabled, bookmarkTagsResolver::fetchTags, bookmarkTagsResolver::generateTagsLocally) :
+                new CircuitBreaker<>(isCircuitBreakerEnabled, bookmarkTagsResolver::fetchTags);
     }
 
     public Bookmark enrichBookmark(String fieldsRequested, Bookmark bookmark) {
         BookmarkFieldSelector fieldSelector = new BookmarkFieldSelector(fieldsRequested);
-        List<String> updatedTags = fieldSelector.enrichTags((b) -> bookmarkTagsResolver.fetchTags(b), bookmark);
+        List<String> updatedTags = fieldSelector.enrichTags((b) -> tagsResolverCircuit.check(b), bookmark);
 
         return Bookmark.builder()
                 .tags(updatedTags)
