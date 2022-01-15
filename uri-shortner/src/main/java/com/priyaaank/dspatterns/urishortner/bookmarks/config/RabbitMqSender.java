ddb129206667bb83a1a6107e0b8f490596300e8c
Priyank Gupta <priyaaank@gmail.com>
2022-01-15 16:58:19 +0530

    Removing RabbitAdmin reference to prevent startup error

--- uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/bookmarks/config/RabbitMqSender.java
+++ uri-shortner/src/main/java/com/priyaaank/dspatterns/urishortner/bookmarks/config/RabbitMqSender.java
@@ -1,28 +1,20 @@
 package com.priyaaank.dspatterns.urishortner.bookmarks.config;
 
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
-
 @Component
 public class RabbitMqSender {
 
-    public static final String QUEUE_MESSAGE_COUNT = "QUEUE_MESSAGE_COUNT";
     private final RabbitTemplate rabbitTemplate;
-    private final RabbitAdmin rabbitAdmin;
 
     @Autowired
-    public RabbitMqSender(RabbitTemplate rabbitTemplate,
-                          RabbitAdmin rabbitAdmin) {
+    public RabbitMqSender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.rabbitAdmin = rabbitAdmin;
     }
 
     public void sendMessage(String message) {
-        Properties queueProperties = rabbitAdmin.getQueueProperties(RabbitMqConfig.queueName);
         this.rabbitTemplate.convertAndSend(RabbitMqConfig.exchangeName, RabbitMqConfig.routingKey, message);
     }
 }
